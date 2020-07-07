857a580ce60d552e753a00b02dee4fa2cf835876
gui2one <guillaume.rouault.fx@gmail.com>
2020-07-08 01:51:40 +0200

    first commit

---
+++ src/core/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Header extends Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+
+        return (
+            <div>Header content</div>
+        )
+    }
+}
+
+export default Header;
